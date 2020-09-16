7934e8ee08768364c0362213078f1bb9bed3b017
Raphael Härtel <raphael.haertel@bluewin.ch>
2020-09-16 12:29:26 +0200

    Script B

--- JsBasics/Task2b.js
+++ JsBasics/Task2b.js
@@ -2,18 +2,16 @@ class Person {
     constructor(firstname, lastname) {
         this._firstname = firstname;
         this._lastname = lastname;
-        this._fullname = firstname + lastname;
     }
 
     _firstname = "";
     _lastname = "";
 
     getFullName() {
-        return _fullname;
+        return this._firstname + this._lastname;
     }
 }
 
 let person = new Person();
 console.log(person);
-
 person.getFullName();
--- JsBasics/Task3b.js
+++ JsBasics/Task3b.js
@@ -1,3 +1,11 @@
-var wordcount = function(text){
+var wordcount = function (text) {
+    var words = [];
 
+
+ 
+
+    // With ES6
+    [text].forEach(c => console.log(c))
 }
+
+wordcount("school is very very very boring that's why i like school");
